73d24408911c4997f9d6f28049388c317617e16c
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2022-01-06 10:20:34 +0100

    Change curl for bloking reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use crate::utils::read_from_toml;
+use curl::easy::Easy2;
 
 mod utils;
 
@@ -8,4 +9,11 @@ fn main() {
     let token = options.get("TOKEN").unwrap();
     let id = options.get("ID").unwrap();
     println!("Token: {}, Id: {}", token, id);
+    let handle = Easy2::new();
+    handle.header_function(|header|{
+        print!("X-Auth-Token: {}", token);
+        print!("X-User-Id: {}", id);
+        print!("Content-type", );
+    })
+
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,50 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
+use std::str::FromStr;
+use curl::easy::{Easy2, Handler, WriteError};
+use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderValue, HeaderName};
+
+struct Collector(Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0.extend_from_slice(data);
+        Ok(data.len())
+    }
+}
+
+pub fn post(url: &str, headers: &HashMap<String, String>)->Result<String, String>{
+    let mut header_map = HeaderMap::new();
+    for keyvalue in headers{
+        header_map.insert(HeaderName::from_str(keyvalue.0).unwrap(),
+                          HeaderValue::from_str(keyvalue.1).unwrap());
+    }
+    let client = Client::builder()
+        .default_headers(header_map)
+        .build()
+        .unwrap();
+    match client.post(url).send(){
+        Ok()
+
+
+    }
+
+}
+
+pub fn get(url: &str)->Result<String, &'static str>{
+    let mut handle = Easy2::new(Collector(Vec::new()));
+    handle.get(true).unwrap();
+    handle.url(url).unwrap();
+    match handle.response_code(){
+        Ok(200) => {
+            let contents = handle.get_ref();
+            Ok(String::from_utf8_lossy(&contents.0).to_string())
+        }
+        Err(e) => Err(&e.to_string())
+    }
+}
 
 
 pub fn read_from_toml(filename: &str)->HashMap<String, String>{
