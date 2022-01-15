4de16cf4ec83d8bfc52187d07283d79c8da3686a
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2022-01-15 10:04:30 +0100

    first interesting version

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,70 @@
-use std::collections::HashMap;
-use crate::utils::read_from_toml;
-use curl::easy::Easy2;
-
+mod bot;
 mod utils;
 
-fn main() {
-    let options: HashMap<String, String> = read_from_toml(".env");
-    let token = options.get("TOKEN").unwrap();
-    let id = options.get("ID").unwrap();
-    println!("Token: {}, Id: {}", token, id);
-    let handle = Easy2::new();
-    handle.header_function(|header|{
-        print!("X-Auth-Token: {}", token);
-        print!("X-User-Id: {}", id);
-        print!("Content-type", );
-    })
+use crate::utils::read_from_toml;
+use crate::bot::Bot;
+use clap::{App, Arg, AppSettings};
 
+const NAME: &str =env!("CARGO_PKG_NAME");
+const DESCRIPTION: &str =env!("CARGO_PKG_DESCRIPTION");
+const VERSION: &str =env!("CARGO_PKG_VERSION");
+const AUTHORS: &str =env!("CARGO_PKG_AUTHORS");
+
+fn main() {
+    let config = read_from_toml(".env");
+    let protocol = config.get("PROTOCOL").unwrap();
+    let base_uri = config.get("BASE_URI").unwrap();
+    let user_id = config.get("USER_ID").unwrap();
+    let token = config.get("ACCESS_TOKEN").unwrap();
+    let bot = Bot::new(protocol, base_uri, user_id, token);
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .author(AUTHORS)
+        .about(DESCRIPTION)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::new("debug")
+             .short('d')
+             .long("debug")
+             .takes_value(false))
+        .subcommand(App::new("list")
+                    .about("List")
+                    .subcommand(App::new("rooms")
+                                .about("List rooms")
+                                )
+                    )
+        .subcommand(App::new("send")
+                    .about("Send")
+                    .subcommand(App::new("message")
+                                .about("Send message")
+                                .arg(Arg::new("room")
+                                     .short('r')
+                                     .required(true)
+                                     .takes_value(true))
+                                .arg(Arg::new("text")
+                                     .short('t')
+                                     .required(true)
+                                     .takes_value(true))
+                                )
+                    .subcommand(App::new("image")
+                                .about("Send image")
+                                )
+                    )
+        .get_matches();
+    if let Some(sub) = matches.subcommand_matches("send"){
+        if let Some(subsub) = sub.subcommand_matches("message"){
+            let room = subsub.value_of("room").unwrap();
+            let text = subsub.value_of("text").unwrap();
+            match bot.send_message(room, text){
+                Ok(result) => println!("{}", result.text().unwrap()),
+                Err(result) => println!("{}", result.to_string())
+            }
+        }
+    }else if let Some(sub) = matches.subcommand_matches("list"){
+        if let Some(subsub) = sub.subcommand_matches("rooms"){
+            match bot.list_rooms(){
+                Ok(result) => println!("{}", result.text().unwrap()),
+                Err(result) => println!("{}", result.to_string())
+            }
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,51 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
-use std::str::FromStr;
-use curl::easy::{Easy2, Handler, WriteError};
-use reqwest::blocking::Client;
-use reqwest::header::{HeaderMap, HeaderValue, HeaderName};
-
-struct Collector(Vec<u8>);
-
-impl Handler for Collector {
-    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
-        self.0.extend_from_slice(data);
-        Ok(data.len())
-    }
-}
-
-pub fn post(url: &str, headers: &HashMap<String, String>)->Result<String, String>{
-    let mut header_map = HeaderMap::new();
-    for keyvalue in headers{
-        header_map.insert(HeaderName::from_str(keyvalue.0).unwrap(),
-                          HeaderValue::from_str(keyvalue.1).unwrap());
-    }
-    let client = Client::builder()
-        .default_headers(header_map)
-        .build()
-        .unwrap();
-    match client.post(url).send(){
-        Ok()
-
-
-    }
-
-}
-
-pub fn get(url: &str)->Result<String, &'static str>{
-    let mut handle = Easy2::new(Collector(Vec::new()));
-    handle.get(true).unwrap();
-    handle.url(url).unwrap();
-    match handle.response_code(){
-        Ok(200) => {
-            let contents = handle.get_ref();
-            Ok(String::from_utf8_lossy(&contents.0).to_string())
-        }
-        Err(e) => Err(&e.to_string())
-    }
-}
-
+use std::io::{BufRead, BufReader};
 
 pub fn read_from_toml(filename: &str)->HashMap<String, String>{
     let mut options:HashMap<String, String> = HashMap::new();
@@ -59,12 +14,5 @@ pub fn read_from_toml(filename: &str)->HashMap<String, String>{
         options.insert(key, value);
     }
     options
-}
-
-pub fn read_from_file(filename: &str) -> String {
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    contents
 }
 
